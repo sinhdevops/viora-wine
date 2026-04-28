0f212335b255a12064119f04e7c623fca7ea1e17
STEMKey <stemkey.edu@gmail.com>
2026-04-28 15:53:52 +0700

    feat: implement product gallery, search functionality, and home page sections with supporting UI components and assets.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -158,13 +158,15 @@ export default async function RootLayout({
 	return (
 		<html lang={locale} className={montserrat.variable} suppressHydrationWarning>
 			<head>
-				<Script id="gtm-head" strategy="afterInteractive">
-					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5P3662VX');`}
-				</Script>
+})(window,document,'script','dataLayer','GTM-5P3662VX');`,
+					}}
+				/>
 				<script
 					type="application/ld+json"
 					dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
